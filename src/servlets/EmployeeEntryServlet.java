ed67be830b400b293561a0fb83aad1691ff43f8a
AndrewMElliott <32204180+AndrewMElliott@users.noreply.github.com>
2017-11-25 21:40:01 -0500

    Jeff's stuff

--- src/servlets/EmployeeEntryServlet.java
+++ src/servlets/EmployeeEntryServlet.java
@@ -9,7 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 
 import utilities.DatabaseAccess;
